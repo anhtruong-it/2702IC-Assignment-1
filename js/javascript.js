eecf0351995ad1caec1abe2636b1334c89d6fc7d
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2024-03-19 22:46:42 +1000

    using flickr api on homepage

--- js/javascript.js
+++ js/javascript.js
@@ -1,38 +1,49 @@
+// initial flickr information
+const apiKey = "api_key=84bcbeb63edc1c2b591367fcc07c81c1";
+const albumId = "photoset_id=72177720315529360";
+const requestAlbumUrl = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&nojsoncallback=1" + "&" + apiKey + "&" + albumId;
+
 $(document).ready(function () {
-    homePage();
+    getHomePageAlbum();
 });
 
-function homePage() {
-    $.get("data/homepagephotos.json", function (data) {
-        var linkList = $("#container");
-        linkList.innerHTML = ""
+// fetch and display homepage photos
+async function getHomePageAlbum() {
+    const linkList = $("#container");
+    linkList.empty();
 
-        for (let i = 0; i < data.length; i++) {
-            var photoBox = $("<div>").addClass("photo-box");
+    try {
+        const response = await fetch(requestAlbumUrl);
+        const data = await response.json();
+        const album = data.photoset.photo;
 
-            var subLink = $("<a class='link-destination'>").attr("href", "pages/destinations.html");
+        $.get("data/homepagephotos.json", function (data) {
+            for (let i = 0; i < album.length; i++) {
+                const photoUrl = `https://farm${album[i].farm}.staticflickr.com/${album[i].server}/${album[i].id}_${album[i].secret}.jpg`;
 
-            subLink.click(function(event) {
-                event.preventDefault();
-                $.cookie("title", encodeURIComponent(data[i].title));
-                $(location).attr("href", "pages/destinations.html");
-            });
+                let photoBox = $("<div>").addClass("photo-box");
 
-            var image = $("<img>").attr("src", "photos/" + data[i].name)
-                .attr("width", 400)
-                .attr("height", 200);
+                let subLink = $("<a class='link-destination'>").attr("href", "pages/destinations.html");
+                subLink.click(function (event) {
+                    event.preventDefault();
+                    $.cookie("title", encodeURIComponent(album[i].title));
+                    $(location).attr("href", "pages/destinations.html");
+                });
 
-            var description = $("<div class='content'>");
+                let image = $("<img>").attr("src", photoUrl).attr("width", 400).attr("height", 200);
+                let description = $("<div class='content'>");
+                let title = $("<h1>").text(album[i].title);
+                let caption = $("<h3>").text(data[i].caption);
 
-            var title = $("<h1>").text(data[i].title);
-            var caption = $("<h3>").text(data[i].caption);
-
-            description.append(title);
-            description.append(caption);
-            subLink.append(image);
-            subLink.append(description);
-            photoBox.append(subLink);
-            linkList.append(photoBox);
-        }
-    });
+                description.append(title);
+                description.append(caption);
+                subLink.append(image);
+                subLink.append(description);
+                photoBox.append(subLink);
+                linkList.append(photoBox);
+            }
+        });
+    } catch (error) {
+        console.error("error fetching album: ", error);
+    }
 }
